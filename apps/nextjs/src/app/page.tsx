8ef5ca104f6d36965ad60fde301ca319ca6d133f
Declan Nishiyama <declan@mountaindev.com>
2024-09-24 19:40:44 -0400

    set up basic api

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,6 +1,4 @@
-import { redirect } from "next/navigation";
-
 export default async function HomePage() {
   // You can await this here if you don't want to show Suspense fallback below
-  redirect("/map");
+  return <div>API</div>;
 }
