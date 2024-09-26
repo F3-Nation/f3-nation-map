d47916a04ef9cec1b20091eba4c2da16ea66200e
King Joshua Montayre <kmontayre07@gmail.com>
2024-09-26 20:19:29 +0800

    Revised implementation on notes link for security purposes

--- apps/nextjs/src/utils/text-link.tsx
+++ apps/nextjs/src/utils/text-link.tsx
@@ -1,15 +1,33 @@
-const urlRegex =
-  /(?:(?:https?|ftp):\/\/)(?:www\.)?[a-z0-9-]+(?:\.[a-z0-9-]+)+[^\s]*/gi;
+import React from "react";
+import DOMPurify from "dompurify";
+
+const urlRegex = /https?:\/\/[^\s]+/g;
 
 export default function textLink(text: string): JSX.Element {
-  const matches = text.match(urlRegex);
+  const parts = text.split(urlRegex);
+  const matches = text.match(urlRegex) ?? [];
 
-  matches?.forEach((match) => {
-    text = text.replace(
-      match,
-      `<a class="underline" href='${match}' target="_blank">${match}</a>`,
-    );
-  });
+  const result = parts.reduce(
+    (arr, part, i) => {
+      arr.push(DOMPurify.sanitize(part));
+      const match = matches[i];
+      if (match) {
+        arr.push(
+          <a
+            key={i}
+            href={matches[i]}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            {DOMPurify.sanitize(match)}
+          </a>,
+        );
+      }
+      return arr;
+    },
+    [] as (string | JSX.Element)[],
+  );
 
-  return <div dangerouslySetInnerHTML={{ __html: text }} />;
+  return <>{result}</>;
 }
