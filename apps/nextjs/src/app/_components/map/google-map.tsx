5547b48348d36d4aac147736e57156725f0aad12
Declan Nishiyama <declan@mountaindev.com>
2025-03-29 18:19:49 -0400

    fix lat lnt and eventId relocation

--- apps/nextjs/src/app/_components/map/google-map.tsx
+++ apps/nextjs/src/app/_components/map/google-map.tsx
@@ -6,16 +6,16 @@ import { useWindowSize } from "@react-hook/window-size";
 import { APIProvider, ControlPosition, Map } from "@vis.gl/react-google-maps";
 
 import { DEFAULT_CENTER, SIDEBAR_WIDTH } from "@acme/shared/app/constants";
-import { safeParseFloat } from "@acme/shared/common/functions";
+import { safeParseFloat, safeParseInt } from "@acme/shared/common/functions";
 import { useTheme } from "@acme/ui/theme";
 import { toast } from "@acme/ui/toast";
 
 import { env } from "~/env";
+import { api } from "~/trpc/react";
 import { useIsMobileWidth } from "~/utils/hooks/use-is-mobile-width";
 import { useUpdateLocSearchParams } from "~/utils/hooks/use-update-loc-search-params";
 import { appStore } from "~/utils/store/app";
 import { mapStore } from "~/utils/store/map";
-import { queryParamStore } from "~/utils/store/query-param";
 import { closePanel, setSelectedItem } from "~/utils/store/selected-item";
 import { MapEventListener } from "../map-event-listener";
 import { MapLayoutItems } from "../map-layout-items";
@@ -60,27 +60,20 @@ const MAP_CONFIGS: MapConfig[] = [
 ];
 
 export const GoogleMapComponent = () => {
+  const utils = api.useUtils();
   const router = useRouter();
   const pathname = usePathname();
   const hasRedirectedForQueryParams = useRef(false);
 
   const searchParams = useSearchParams();
   const queryLat = safeParseFloat(searchParams?.get("lat"));
-  const queryLon = safeParseFloat(searchParams?.get("lon"));
+  const queryLon = safeParseFloat(
+    searchParams?.get("lon") ?? searchParams?.get("lng"),
+  );
   const queryZoom = safeParseFloat(searchParams?.get("zoom"));
-  const queryLocationId = searchParams?.get("locationId");
-  const queryEventId = searchParams?.get("eventId");
+  const queryLocationId = safeParseInt(searchParams?.get("locationId"));
+  const queryEventId = safeParseInt(searchParams?.get("eventId"));
   const searchParamsRef = useRef(searchParams?.get("error"));
-
-  useEffect(() => {
-    queryParamStore.setState({
-      lat: queryLat,
-      lon: queryLon,
-      zoom: queryZoom,
-      locationId: queryLocationId ? Number(queryLocationId) : null,
-      eventId: queryEventId ? Number(queryEventId) : null,
-    });
-  }, [queryLat, queryLon, queryZoom, queryLocationId, queryEventId]);
 
   useEffect(() => {
     const error = searchParamsRef.current;
@@ -96,8 +89,8 @@ export const GoogleMapComponent = () => {
   // This needs to just be an onload thing TODO
   useEffect(() => {
     if (
-      queryLocationId &&
-      queryEventId &&
+      queryLocationId != null &&
+      queryEventId != null &&
       !hasRedirectedForQueryParams.current
     ) {
       hasRedirectedForQueryParams.current = true;
@@ -112,97 +105,138 @@ export const GoogleMapComponent = () => {
   return (
     <Suspense>
       <APIProvider apiKey={env.NEXT_PUBLIC_GOOGLE_API_KEY}>
-        <ProvidedGoogleMapComponent />
+        <ProvidedGoogleMapComponent
+          defaultZoom={getDefaultZoom({
+            queryZoom: queryZoom,
+          })}
+          defaultCenter={getDefaultCenter({
+            locationData: utils.location.getLocationMarkersSparse.getData(),
+            queryLat,
+            queryLon,
+            queryLocationId,
+          })}
+        />
       </APIProvider>
     </Suspense>
   );
 };
 
-const ProvidedGoogleMapComponent = memo(() => {
-  console.log("ProvidedGoogleMapComponent rerender");
+const ProvidedGoogleMapComponent = memo(
+  ({
+    defaultZoom,
+    defaultCenter,
+  }: {
+    defaultZoom: number;
+    defaultCenter: google.maps.LatLngLiteral;
+  }) => {
+    console.log("ProvidedGoogleMapComponent rerender");
 
-  const { isMobileWidth } = useIsMobileWidth();
-  const { resolvedTheme } = useTheme();
+    const { isMobileWidth } = useIsMobileWidth();
+    const { resolvedTheme } = useTheme();
 
-  const tiles = mapStore.use.tiles();
-  const mapTiles = MAP_CONFIGS.find((map) =>
-    tiles !== "street"
-      ? map.id === tiles
-      : resolvedTheme === "dark"
-        ? map.id === "dark"
-        : map.id === "light",
-  );
+    const tiles = mapStore.use.tiles();
+    const mapTiles = MAP_CONFIGS.find((map) =>
+      tiles !== "street"
+        ? map.id === tiles
+        : resolvedTheme === "dark"
+          ? map.id === "dark"
+          : map.id === "light",
+    );
 
-  const [width, height] = useWindowSize();
+    const [width, height] = useWindowSize();
 
-  // Initialize selected item from URL params
+    // Initialize selected item from URL params
 
-  return (
-    <div
-      style={{
-        height,
-        width: width >= 1024 ? width - SIDEBAR_WIDTH : width,
-      }}
-    >
-      <Map
-        mapId={mapTiles?.mapId}
-        mapTypeId={mapTiles?.mapTypeId}
-        styles={mapTiles?.styles}
-        defaultZoom={getDefaultZoom()}
-        defaultCenter={getDefaultCenter()}
-        gestureHandling="greedy"
-        streetViewControl={false}
-        mapTypeControl={false}
-        fullscreenControl={false}
-        zoomControl={true}
-        zoomControlOptions={{
-          position: isMobileWidth
-            ? ControlPosition.RIGHT_TOP
-            : ControlPosition.RIGHT_BOTTOM,
+    return (
+      <div
+        style={{
+          height,
+          width: width >= 1024 ? width - SIDEBAR_WIDTH : width,
         }}
-        cameraControl={false}
-        disableDoubleClickZoom
-        // styles={[ { featureType: "all", elementType: "all", stylers: [{ visibility: "off" }], }, ]}
-        onClick={(e) => {
-          console.log("map on click", e);
-          if (appStore.get("mode") === "edit" && e.detail.latLng) {
-            const latLng = e.detail.latLng;
-            mapStore.setState({ updateLocation: latLng });
-          } else {
-            setSelectedItem({
-              locationId: null,
-              eventId: null,
-              showPanel: false,
-            });
-            closePanel();
-          }
-        }}
-        clickableIcons={false}
       >
-        <UrlUpdater />
-        <ClusteredMarkers />
-        <MapLayoutItems />
-        <UpdatePane />
-        <PlaceResultIconPane />
-        <MapEventListener />
-      </Map>
-    </div>
-  );
-});
+        <Map
+          mapId={mapTiles?.mapId}
+          mapTypeId={mapTiles?.mapTypeId}
+          styles={mapTiles?.styles}
+          defaultZoom={defaultZoom}
+          defaultCenter={defaultCenter}
+          gestureHandling="greedy"
+          streetViewControl={false}
+          mapTypeControl={false}
+          fullscreenControl={false}
+          zoomControl={true}
+          zoomControlOptions={{
+            position: isMobileWidth
+              ? ControlPosition.RIGHT_TOP
+              : ControlPosition.RIGHT_BOTTOM,
+          }}
+          cameraControl={false}
+          disableDoubleClickZoom
+          // styles={[ { featureType: "all", elementType: "all", stylers: [{ visibility: "off" }], }, ]}
+          onClick={(e) => {
+            console.log("map on click", e);
+            if (appStore.get("mode") === "edit" && e.detail.latLng) {
+              const latLng = e.detail.latLng;
+              mapStore.setState({ updateLocation: latLng });
+            } else {
+              setSelectedItem({
+                locationId: null,
+                eventId: null,
+                showPanel: false,
+              });
+              closePanel();
+            }
+          }}
+          clickableIcons={false}
+        >
+          <UrlUpdater />
+          <ClusteredMarkers />
+          <MapLayoutItems />
+          <UpdatePane />
+          <PlaceResultIconPane />
+          <MapEventListener />
+        </Map>
+      </div>
+    );
+  },
+  () => {
+    return false;
+  },
+);
 
 ProvidedGoogleMapComponent.displayName = "ProvidedGoogleMapComponent";
 
 export default GoogleMapComponent;
 
-const getDefaultCenter = () => {
-  const queryLat = queryParamStore.get("lat");
-  const queryLon = queryParamStore.get("lon");
+const getDefaultCenter = ({
+  queryLat,
+  queryLon,
+  queryLocationId,
+  locationData,
+}: {
+  queryLat: number | undefined;
+  queryLon: number | undefined;
+  queryLocationId: number | undefined;
+  locationData:
+    | {
+        id: number;
+        lat: number | null;
+        lon: number | null;
+      }[]
+    | undefined;
+}) => {
+  const locationLatLng = locationData?.find(
+    (location) => location.id === queryLocationId,
+  );
   const center = mapStore.get("center");
-  return queryLat != null && queryLon != null
-    ? { lat: queryLat, lng: queryLon }
-    : center
-      ? center
-      : { lat: DEFAULT_CENTER[0], lng: DEFAULT_CENTER[1] };
+
+  return locationLatLng?.lat != null && locationLatLng.lon != null
+    ? { lat: locationLatLng.lat, lng: locationLatLng.lon }
+    : queryLat != null && queryLon != null
+      ? { lat: queryLat, lng: queryLon }
+      : center
+        ? center
+        : { lat: DEFAULT_CENTER[0], lng: DEFAULT_CENTER[1] };
 };
 
 const UrlUpdater = () => {
@@ -210,8 +244,7 @@ const UrlUpdater = () => {
   return null;
 };
 
-const getDefaultZoom = () => {
-  const queryZoom = queryParamStore.get("zoom");
+const getDefaultZoom = ({ queryZoom }: { queryZoom: number | undefined }) => {
   const zoom = mapStore.get("zoom");
   return queryZoom ?? zoom;
 };
