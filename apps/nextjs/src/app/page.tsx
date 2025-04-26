827703cbe36c8b290e462ade9c69ea063782c55d
Declan Nishiyama <declan@mountaindev.com>
2025-04-25 22:02:50 -0400

    update imports

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -3,13 +3,13 @@ import { Suspense } from "react";
 import { RERENDER_LOGS } from "@acme/shared/common/constants";
 
 import { MapPageWrapper } from "~/app/_components/map-page-wrapper";
+import { FilteredMapResultsProvider } from "~/app/_components/map/filtered-map-results-provider";
 import { GoogleMapComponent } from "~/app/_components/map/google-map";
+import { InitialLocationProvider } from "~/app/_components/map/initial-location-provider";
+import { ReactQueryHydrator } from "~/app/_components/map/react-query-hydrator";
+import { TextSearchResultsProvider } from "~/app/_components/map/search-results-provider";
 import { ssg } from "~/trpc/ssg";
 import { TouchDeviceProvider } from "~/utils/touch-device-provider";
-import { FilteredMapResultsProvider } from "./_components/map/filtered-map-results-provider";
-import { InitialLocationProvider } from "./_components/map/initial-location-provider";
-import { ReactQueryHydrator } from "./_components/map/react-query-hydrator";
-import { TextSearchResultsProvider } from "./_components/map/search-results-provider";
 
 export default async function MapPage() {
   const mapEventAndLocationData =
