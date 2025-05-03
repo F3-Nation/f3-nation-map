da652626d89a076d211cc41ea131d84d5ed34cff
Declan Nishiyama <declan@mountaindev.com>
2025-05-03 16:04:53 -0400

    bump nextjs to 15.3

--- apps/nextjs/src/app/admin/users/[id]/page.tsx
+++ apps/nextjs/src/app/admin/users/[id]/page.tsx
@@ -1,11 +1,10 @@
 import { api } from "~/trpc/server";
 import UserMutate from "./user-mutate";
 
-export default async function UserEditPage({
-  params,
-}: {
-  params: { id: string };
+export default async function UserEditPage(props: {
+  params: Promise<{ id: string }>;
 }) {
+  const params = await props.params;
   const user = await api.user.byId({ id: Number(params.id) });
   return <UserMutate user={user} />;
 }
