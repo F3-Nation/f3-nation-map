09bd5f0ed4b3d3130195ee1d2e7fccea4b49841f
Declan Nishiyama <declan@mountaindev.com>
2025-05-10 11:06:10 -0400

    remove sentry outside production

--- apps/nextjs/sentry.edge.config.ts
+++ apps/nextjs/sentry.edge.config.ts
@@ -6,15 +6,16 @@ import * as Sentry from "@sentry/nextjs";
 
 import { env } from "~/env";
 
-Sentry.init({
-  dsn: "https://7174fea65c117ea4b71977da953bb4d9@o4509266839797760.ingest.us.sentry.io/4509270283714560",
-  enabled: env.NODE_ENV === "production",
+if (env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://7174fea65c117ea4b71977da953bb4d9@o4509266839797760.ingest.us.sentry.io/4509270283714560",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  environment: env.NEXT_PUBLIC_CHANNEL,
-});
+    environment: env.NEXT_PUBLIC_CHANNEL,
+  });
+}
