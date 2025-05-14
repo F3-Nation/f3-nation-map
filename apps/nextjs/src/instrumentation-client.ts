d111eed1e969dfd9e870552a61c76a88fcef0dbb
Declan Nishiyama <declan@mountaindev.com>
2025-05-14 07:23:26 -0400

    update sentry for using environment

--- apps/nextjs/src/instrumentation-client.ts
+++ apps/nextjs/src/instrumentation-client.ts
@@ -7,6 +7,7 @@ import * as Sentry from "@sentry/nextjs";
 import { env } from "~/env";
 
 if (env.NODE_ENV === "production") {
+  const channel = env.NEXT_PUBLIC_CHANNEL;
   Sentry.init({
     dsn: "https://7174fea65c117ea4b71977da953bb4d9@o4509266839797760.ingest.us.sentry.io/4509270283714560",
 
@@ -31,6 +32,13 @@ if (env.NODE_ENV === "production") {
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
+
+    environment:
+      channel === "prod"
+        ? "production"
+        : channel === "staging"
+          ? "staging"
+          : "development",
   });
 }
 
